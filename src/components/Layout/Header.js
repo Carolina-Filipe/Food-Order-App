819b54a7be4f272e834d1ffc8161d3cab935f62a
Carolina Filipe <anilorac.filipe@gmail.com>
2022-08-08 18:21:43 +0200

    add context

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { useContext, Fragment } from "react";
 
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
